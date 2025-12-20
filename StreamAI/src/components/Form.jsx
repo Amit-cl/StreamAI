14f5e3f891ecf46fae6dd4bfe0db0b79b3614c52
rahul <animenew122333@gmail.com>
2025-12-20 11:57:37 +0530

    header-completed

--- StreamAI/src/components/Form.jsx
+++ StreamAI/src/components/Form.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Form = () => {
   return (
-    <div>Form</div>
+    <div>
+      <img
+      src = "https://assets.nflxext.com/ffe/siteui/vlv3/6d631aa6-567d-46ef-a644-b5b00e4334d2/web/IN-en-20251215-TRIFECTA-perspective_f1cab02a-e42b-4913-a7d9-c5fe0f94f68d_large.jpg"
+      alt = 'body image'
+      />
+    </div>
   )
 }
 
--- StreamAI/src/components/Header.jsx
+++ StreamAI/src/components/Header.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div >Header</div>
+     <div className='absolute px-8 py-2 bg-linear-to-b from-black w-full'>
+      <svg width="350" height="100" viewBox="0 0 350 100" xmlns="http://www.w3.org/2000/svg" 
+      className='w-44'
+      >
+  <text x="10" y="70" font-family="Helvetica, Arial, sans-serif" font-weight="900" font-size="65" fill="#E50914" style={{ letterSpacing: '-3px' }}>
+    StreamAi
+  </text>
+</svg>
+    </div>
   )
 }
 
