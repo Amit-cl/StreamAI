96e202201471410ab8f0df03f369a55659d35321
rahul <animenew122333@gmail.com>
2025-12-20 12:12:41 +0530

    svgr-installed/initialized

--- StreamAI/src/components/Header.jsx
+++ StreamAI/src/components/Header.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
+import StreamAiLogo from "../assets/icons/streamAiLogo.svg?react"
 
 const Header = () => {
   return (
      <div className='absolute px-8 py-2 bg-linear-to-b from-black w-full'>
-      <svg width="350" height="100" viewBox="0 0 350 100" xmlns="http://www.w3.org/2000/svg" 
-      className='w-44'
-      >
-  <text x="10" y="70" font-family="Helvetica, Arial, sans-serif" font-weight="900" font-size="65" fill="#E50914" style={{ letterSpacing: '-3px' }}>
-    StreamAi
-  </text>
-</svg>
+    <StreamAiLogo/>
     </div>
   )
 }
