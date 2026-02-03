715c387fd81c8c3acfd103383f8e1c1b4bc38a9d
rahul <animenew122333@gmail.com>
2026-02-03 15:46:53 +0530

    secure firebase config

--- StreamAI/src/utility/firebase.js
+++ StreamAI/src/utility/firebase.js
@@ -6,13 +6,13 @@ import { getAuth , GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDROiPWvWB71YYMea6rbLgwOv2dtkZ8jE0",
-  authDomain: "streamai-c1d17.firebaseapp.com",
-  projectId: "streamai-c1d17",
-  storageBucket: "streamai-c1d17.firebasestorage.app",
-  messagingSenderId: "821174413666",
-  appId: "1:821174413666:web:77de9ffb6da1a7d133a43b"
-};
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
